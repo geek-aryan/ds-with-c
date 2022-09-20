5fc0a5f511d146066c2921426828c7103970813d
Aryan <aryanbawankar22@gmail.com>
2022-09-21 02:13:04 +0530

    added revers linked list func

--- Self_LinkedList.c
+++ Self_LinkedList.c
@@ -5,14 +5,23 @@ struct node
     int data;
     struct node* next;
 };
+struct Stack
+{
+    int arr[10];
+    int tos;
+};
 void append(struct node**,int);
 void display(struct node*);
+void reverse_display(struct node*);
+void lreverse(struct node**);
 int count_node(struct node*);
 int search_node(struct node*,int);
 void del_first(struct node**);
 void del_last(struct node**);
 void del_any(struct node**,int);
 void insert(struct node**,int);
+void push(struct Stack*,int);
+int pop(struct Stack*);
 int main()
 {
     struct node* start=NULL;
@@ -24,11 +33,14 @@ int main()
         insert(&start,x);
     }
     display(start);
-    printf("Enter data to delete into the list: ");
-    scanf("%d",&x);
-    del_any(&start,x);
-    printf("After delete list is:\n");
+    printf("After reversing list then list is:\n");
+    lreverse(&start);
     display(start);
+//    printf("Enter data to delete into the list: ");
+//    scanf("%d",&x);
+//    del_any(&start,x);
+//    printf("After delete list is:\n");
+//    display(start);
     return 0;
 }
 void append(struct node** ps,int x)
@@ -61,6 +73,57 @@ void display(struct node* p)
         printf("%d\n",p->data);
         p=p->next;
     }
+}
+void reverse_display(struct node* p)
+{
+    if(p==NULL)
+    {
+        printf("List is Empty\n");
+        return;
+    }
+    struct Stack s;
+    s.tos=-1;
+    while(p!=NULL)
+    {
+//        printf("%d\n",p->data);
+        push(&s,p->data);
+        p=p->next;
+    }
+    while(s.tos!=-1)
+    {
+        printf("%d\n",pop(&s));
+    }
+
+}
+void lreverse(struct node** ps)
+{
+    struct node *temp,*prev,*rtemp,*rprev;
+    if(*ps==NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    if((*ps)->next==NULL)
+        return;
+    temp=*ps;
+    while(temp->next!=NULL)
+    {
+        prev=temp;
+        temp=temp->next;
+        if(*ps==prev)
+        {
+            rtemp=prev;
+            rtemp->next=NULL;
+            rprev=temp;
+        }
+        else{
+            rprev->next=rtemp;
+            rprev=temp;
+            rtemp=prev;
+        }
+    }
+    temp->next=prev;
+    *ps=temp;
 }
 int count_node(struct node* p)
 {
@@ -203,4 +266,26 @@ void insert(struct node** ps,int x)
         return;
     }
     temp->next=p;
+}
+void push(struct Stack* s,int x)
+{
+    if(s->tos==9)
+    {
+        printf("Stack is overflow\n");
+        return;
+    }
+    s->tos++;
+    s->arr[s->tos]=x;
+//    printf("%d element is pushed in stack\n",x);
+}
+int pop(struct Stack* s)
+{
+    if(s->tos==-1)
+    {
+        printf("Stack is underflow\n");
+        return -1;
+    }
+    int x=s->arr[s->tos];
+    s->tos--;
+    return x;
 }
